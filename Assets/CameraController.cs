4efc2136e890c43bbc1275d6dc52e259fb00cac6
Quangpromen <lenhatquang27112003@gmail.com>
2025-02-18 16:42:29 +0700

    gioi dan bien

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform target;
+    public Vector3 offset;
+    [Range(1, 10)]
+    public float smoothFactor;
+    public Vector3 minValues, maxValues;
+    public float minY, maxY; // Add these lines for upper and lower bounds
+    private void FixedUpdate()
     {
-        
+        Follow();
     }
-
-    // Update is called once per frame
-    void Update()
+    void Follow()
     {
-        
+        Vector3 targetPosition = target.position + offset;
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValues.x),
+            Mathf.Clamp(targetPosition.y, Mathf.Clamp(targetPosition.y, minY, maxY), maxValues.y), // Apply the Y bounds
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValues.z)
+            );
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = smoothPosition;
     }
 }
---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float speed = 5f; // Tốc độ di chuyển
+
+    // Giới hạn biên theo tọa độ bạn cung cấp
+    private float minX = -7.14f, maxX = 8.34f;
+    private float minY = -5.05f, maxY = 4.88f;
+
+    void Update()
+    {
+        MovePlayer();
+        ClampPosition();
+    }
+
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position += new Vector3(moveX, moveY, 0);
+    }
+
+    void ClampPosition()
+    {
+        Vector3 pos = transform.position;
+
+        // Giữ Player trong giới hạn biên
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
+    }
+}
